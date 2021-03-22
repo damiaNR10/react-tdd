955f25939336d2fbac1024924f711eefa53eb987
damiaNR10 <damianrog96@gmail.com>
2021-03-22 23:57:19 +0100

    Test Driven Development example

--- src/RomanConverter.test.js
+++ src/RomanConverter.test.js
@@ -1,10 +1,24 @@
-import {cleanup, render, screen} from '@testing-library/react';
+import {cleanup, fireEvent, render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react'
 
 class RomanConverter extends React.Component {
+
+    state = {
+        roman: null,
+    }
+
+    handleChange = (event) => {
+        this.setState({roman: "I"});
+    }
+
     render() {
-        return <label>Arabic: <input type="number" /></label>;
+        return (
+            <>
+            <label>Arabic: <input onChange = {this.handleChange} type="number" /></label>;
+            <h1>Roman: {this.state.roman ? this.state.roman : "none"}</h1>
+            </>
+        )
     }
 }
 
@@ -20,6 +34,19 @@ describe("<RomanConverter />", () => {
     });
 
     it('show no roman number by default', () => {
-        
+        const {getByText} = render(<RomanConverter />);
+
+        expect(() => {
+            getByText("Roman: none");
+        }).not.toThrow();
+    });
+
+    it('converts 1 to I', () => {
+        const {getByText, getByLabelText} = render(<RomanConverter />);
+        fireEvent.change(getByLabelText(/arabic/i), {target: {value: "1"}});
+
+        expect(() => {
+            getByText("Roman: I");
+        }).not.toThrow();
     });
 })
